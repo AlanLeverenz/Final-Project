af6faedb4bbdeeb5e6a7dd912847b544eddd194c
Alan Leverenz <awleverenz@aol.com>
2020-02-15 00:10:30 -0500

    prepped for Heroku deployment

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI_ || "mongodb://localhost/mongoSentiment", 
+  process.env.MONGODB_URI || "mongodb://localhost/mongoSentiment", 
   { useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
